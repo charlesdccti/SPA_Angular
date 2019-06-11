fc7498ba8f6e173d13a6814f44003c9492ad0e5f
CHARLES FERREIRA <charlesdccti@gmail.com>
2019-06-11 10:13:18 -0300

    Capturando eventos e o Output

--- src/app/barra-navegacao/menu-lateral/menu-lateral.component.ts
+++ src/app/barra-navegacao/menu-lateral/menu-lateral.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuLateralComponent implements OnInit {
 
+  menuAberto = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onToggle(){
+    this.menuAberto = !this.menuAberto;
   }
 
 }
